66b9f33fff728eed682088fcfac20eac35589810
Vitor Parras <vitorhugoparras@hotmail.com>
2023-02-13 15:31:57 -0300

    Login Component Completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './pages/login/login.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon'
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MenuComponent } from './pages//menu/menu.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BaseComponent } from './pages/shared/base/base.component';
+import { AngularTiltModule } from 'angular-tilt';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    HomeComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    AngularTiltModule
   ],
   providers: [],
   bootstrap: [AppComponent]
